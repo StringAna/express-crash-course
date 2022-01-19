31242b5ed706da846b636e8255be5e4831a1af8d
StringAna <tewaryantara21@gmail.com>
2022-01-19 18:10:43 +0530

    sending index.html to server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from express</h1>');
+  //   res.send('<h1>Hello from express</h1>');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 //to run the web server on a port
