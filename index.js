dc37cd88a315b5ff375ecf7f0db568a4fee4a6c5
StringAna <tewaryantara21@gmail.com>
2022-01-19 18:33:07 +0530

    separating members var

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const members = require('./Members');
 
 // app.get('/', (req, res) => {
 //   res.send('<h1>Hello from express</h1>');
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
-
-//create object, send it as response in json format
-//hit it with postman
-const members = [
-  {
-    id: 1,
-    name: 'john doe',
-    email: 'john@gmail.com',
-    status: 'active',
-  },
-  {
-    id: 2,
-    name: 'cena doe',
-    email: 'cena@gmail.com',
-    status: 'inactive',
-  },
-  {
-    id: 3,
-    name: 'pena poe',
-    email: 'pena@gmail.com',
-    status: 'inactive',
-  },
-];
 
 //gets all members
 app.get('/api/members', (req, res) => {
