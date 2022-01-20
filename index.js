ef0e29f2c30024b0cd1283c4bfa3ba834c6ea021
StringAna <tewaryantara21@gmail.com>
2022-01-20 15:21:30 +0530

    ussing http status codes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { param } = require('express/lib/request');
 const path = require('path');
 const members = require('./Members');
 const logger = require('./middleware/logger');
@@ -15,9 +16,17 @@ app.get('/api/members', (req, res) => {
 
 //get single member
 app.get('/api/members/:id', (req, res) => {
+  //to deal with members that don't exist
+  const found = members.some((mem) => mem.id === parseInt(req.params.id));
+
   // === : data type has to match on both sides
   // parseInt to convert id to int
-  res.json(members.filter((mem) => mem.id === parseInt(req.params.id)));
+  if (found) {
+    res.json(members.filter((mem) => mem.id === parseInt(req.params.id)));
+  } else {
+    //http-status 400 : bad req
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 //set static folder
