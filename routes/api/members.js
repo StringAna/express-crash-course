b35f30b4932233b4274219b671e505ce78ca3516
StringAna <tewaryantara21@gmail.com>
2022-01-26 10:21:46 +0530

    update a member-PUT request

--- routes/api/members.js
+++ routes/api/members.js
@@ -44,4 +44,32 @@ router.post('/', (req, res) => {
   res.json(members);
 });
 
+//update member
+//to update something on the server , we use PUT request
+router.put('/:id', (req, res) => {
+  //to deal with members that don't exist
+  const found = members.some((mem) => mem.id === parseInt(req.params.id));
+
+  // === : data type has to match on both sides
+  // parseInt to convert id to int
+  if (found) {
+    //put the body , what you want to send in updateMember
+    const updateMember = req.body;
+    //loop through the member to see if the id of members array mathes the one that is passed in the params
+    members.forEach((mem) => {
+      if (mem.id === parseInt(req.params.id)) {
+        //check to see if the name and email are actually sent,if not then set the name to the old name
+        mem.name = updateMember.name ? updateMember.name : mem.name;
+        mem.email = updateMember.email ? updateMember.email : mem.email;
+
+        //send back a response
+        res.json({ msg: 'member updated', mem });
+      }
+    });
+  } else {
+    //http-status 400 : bad req
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
 module.exports = router;
