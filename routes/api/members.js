192d85580a4079aa1eabca8f7004757ce1b10ffe
StringAna <tewaryantara21@gmail.com>
2022-01-26 09:54:45 +0530

    adding a new member to members array

--- routes/api/members.js
+++ routes/api/members.js
@@ -1,7 +1,7 @@
 const members = require('../../Members');
 const express = require('express');
 const router = express.Router();
-
+const uuid = require('uuid');
 //gets all members
 router.get('/', (req, res) => {
   res.json(members);
@@ -25,7 +25,23 @@ router.get('/:id', (req, res) => {
 //create a member - POST request
 router.post('/', (req, res) => {
   //send data, and when we get data, it should be in the req object
-  res.send(req.body);
+  // res.send(req.body);
+
+  //creating a new member
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active',
+  };
+  //add this new member to an array,make sure that name and email are sent with the request
+  if (!newMember.name || !newMember.email) {
+    //bad request 400
+    return res.status(400).json({ msg: 'please include a name and email' });
+  }
+  members.push(newMember);
+  //respond with array of memebers including new member
+  res.json(members);
 });
 
 module.exports = router;
